14838850d9bf86eb1068196378573d9d304fd03f
Prodipta0109 <prodiptasenamartya1971@gmail.com>
2021-06-22 03:15:04 +0600

    First Merge (from-to)

--- app/src/main/java/com/example/bus/fragments/HomeFragment.java
+++ app/src/main/java/com/example/bus/fragments/HomeFragment.java
@@ -1,5 +1,7 @@
 package com.example.bus.fragments;
 
+import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,11 +9,19 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.example.bus.Buslist;
 import com.example.bus.R;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+
+import java.util.Calendar;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -22,10 +32,12 @@ import de.hdodenhof.circleimageview.CircleImageView;
  */
 public class HomeFragment extends Fragment {
     View view;
-    private TextView fromText,toText,dateText;
-    private EditText fromEnterEditText, toEneterEdittext,dateEdittext;
     private Button searchButton;
-    private CircleImageView imageView;
+    private AutoCompleteTextView fromAutoCompleteTextView,toAutoCompleteTextView;
+    private TextInputEditText journeyDateEditText;
+    private TextInputLayout textInputLayout;
+    private int journeyDate,journeyMonth,journeyYear;
+
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -72,14 +84,70 @@ public class HomeFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         view = inflater.inflate(R.layout.fragment_home, container, false);
-        fromText = view.findViewById(R.id.fromTxt);
-        toText = view.findViewById(R.id.toTxt);
-        fromEnterEditText = view.findViewById(R.id.fromEntercity);
-        toEneterEdittext = view.findViewById(R.id.toEnterCity);
-        dateText = view.findViewById(R.id.dateText);
-        dateEdittext = view.findViewById(R.id.pickDate);
-        imageView = view.findViewById(R.id.circleImage);
-        searchButton = view.findViewById(R.id.searchBusButton);
+        fromAutoCompleteTextView = view.findViewById(R.id.from_auto_completeTextViewId);
+        toAutoCompleteTextView  = view.findViewById(R.id.to_auto_completeTextViewId);
+        journeyDateEditText = view.findViewById(R.id.journeydate_editTextViewId);
+        textInputLayout = view.findViewById(R.id.textInputLayoutId);
+        searchButton = view.findViewById(R.id.search_busesId);
+        String[] from = getResources().getStringArray(R.array.from);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getActivity(),R.layout.dropdown_item,from);
+        fromAutoCompleteTextView.setAdapter(arrayAdapter);
+
+        String[] to = getResources().getStringArray(R.array.from);
+        ArrayAdapter<String> arrayAdapter1 = new ArrayAdapter<String>(getActivity(),R.layout.dropdown_item,to);
+        toAutoCompleteTextView.setAdapter(arrayAdapter);
+
+        journeyDateEditText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Calendar calendar = Calendar.getInstance();
+                journeyDate = calendar.get(Calendar.DATE);
+                journeyMonth = calendar.get(Calendar.MONTH);
+                journeyYear = calendar.get(Calendar.YEAR);
+
+                DatePickerDialog datePickerDialog = new DatePickerDialog(getActivity(),
+                        android.R.style.Theme_DeviceDefault_Dialog,
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker datePicker, int year, int month, int date) {
+                                journeyDateEditText.setText(date+" - "+month+" - "+year);
+                            }
+                        },
+                        journeyYear,
+                        journeyMonth,
+                        journeyDate);
+                datePickerDialog.getDatePicker().setMaxDate(System.currentTimeMillis()-1000);
+                datePickerDialog.show();
+            }
+        });
+
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String fromCity = fromAutoCompleteTextView.getText().toString().toLowerCase();
+                String toCity = toAutoCompleteTextView.getText().toString().toLowerCase();
+                String date = journeyDateEditText.getText().toString().toLowerCase();
+                if(fromCity.isEmpty()){
+                    fromAutoCompleteTextView.setError("From City is Required");
+                    fromAutoCompleteTextView.requestFocus();
+                    return;
+                }
+                if(toCity.isEmpty()){
+                    toAutoCompleteTextView.setError("To City is required");
+                    toAutoCompleteTextView.requestFocus();
+                    return;
+                }
+                if(date.isEmpty()){
+                    journeyDateEditText.setError("Journey date is required");
+                    journeyDateEditText.requestFocus();
+                    return;
+                }
+                Intent intent = new Intent(getActivity(), Buslist.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+            }
+        });
+
         return view;
     }
 }
